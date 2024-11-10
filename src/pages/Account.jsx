e4ebcaa6bd05d5be70a1de013750f33bd6d8cdc1
Lorena Suárez <30444782+lorelero@users.noreply.github.com>
2024-11-10 17:21:21 -0300

    Actualizacion account

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,18 +1,23 @@
-
+import React from "react";
 import RegisterForm from "../components/Auth/Register";
 import LoginForm from "../components/Auth/Login";
+import { Container, Row, Col } from "react-bootstrap";
+import "../styles.css"; // Asegúrate de tener este archivo para estilos personalizados
 
 const Account = () => {
-    return (
-      <div className="account-container">
-        <div className="form-container">
+  return (
+    <Container className="account-container mt-5">
+      <Row className="justify-content-center align-items-start">
+        <Col md={5} className="mb-4">
           <RegisterForm />
-        </div>
-        <div className="form-container">
+        </Col>
+        <div className="vertical-line"></div>
+        <Col md={5} className="mb-4">
           <LoginForm />
-        </div>
-      </div>
-    );
-  };
-  
-  export default Account;
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Account;
