6c6c8e5c6da48083b7450ac1af6aeeeb6ee52b20
Lorena Suárez <30444782+lorelero@users.noreply.github.com>
2024-11-10 17:20:26 -0300

    actualizacion de login y register

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,45 +1,50 @@
-import {useContext, useState } from 'react';
+import React, { useContext, useState } from "react";
+import { Form, Button, Card } from "react-bootstrap";
 import { AuthContext } from "../../context/AuthProvider";
 
-const Login = () => {
+const LoginForm = () => {
   const { login } = useContext(AuthContext);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-  const handleSubmit = (e) => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmitLogin = (e) => {
     e.preventDefault();
     login(email, password);
-    console.log('Login:', { email, password });
+    console.log('Login:', formData);
   };
 
   return (
-    <div className="container mt-5">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">Ingresar</button>
-      </form>
-    </div>
+    <Card className="card-custom">
+      <Card.Body>
+        <Card.Title className="text-center">Ya Tengo Cuenta</Card.Title>
+        <Form onSubmit={handleSubmitLogin}>
+          {/* Campos del formulario */}
+          <Form.Group controlId="formEmailLogin" className="mb-3">
+            <Form.Control
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              placeholder="Ingresa tu correo electrónico"
+              className="border-lila"
+            />
+          </Form.Group>
+          {/* Agrega los demás campos */}
+          <Button variant="dark" type="submit" className="button-custom mt-3">
+            Iniciar Sesión
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
   );
 };
 
-export default Login;
+export default LoginForm;
