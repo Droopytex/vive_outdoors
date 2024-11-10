8d8c954efdc22285aa30f17506e8d152a4b97bf7
Lorena Suárez <30444782+lorelero@users.noreply.github.com>
2024-11-10 17:56:59 -0300

    subida 2

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,22 +1,16 @@
-import React from "react";
-import RegisterForm from "../components/Auth/Register";
-import LoginForm from "../components/Auth/Login";
-import { Container, Row, Col } from "react-bootstrap";
-import "../styles.css"; // Asegúrate de tener este archivo para estilos personalizados
+import Register from "../components/Auth/Register";
+import Login from "../components/Auth/Login";
 
 const Account = () => {
   return (
-    <Container className="account-container mt-5">
-      <Row className="justify-content-center align-items-start">
-        <Col md={5} className="mb-4">
-          <RegisterForm />
-        </Col>
-        <div className="vertical-line"></div>
-        <Col md={5} className="mb-4">
-          <LoginForm />
-        </Col>
-      </Row>
-    </Container>
+    <div className="account-container">
+      <div className="form-container">
+        <Register />
+      </div>
+      <div className="form-container">
+        <Login />
+      </div>
+    </div>
   );
 };
 
