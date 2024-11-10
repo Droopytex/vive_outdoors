9c57bb72996c57b1cfce93e5f8e4b15d637ca2c6
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-11-10 11:25:18 -0300

    funciones agregarCarrito, sumarCantidad, restarCantidad, calcularTotal, actualización return

--- src/context/viveProvider.jsx
+++ src/context/viveProvider.jsx
@@ -18,13 +18,50 @@ const viveOutdoorProvider = ({ children }) => {
     console.log(producto); // la consola muestra la actualización del producto
   };
 
-  return (
+  function agregarCarrito({ id_producto, nombre, precio, foto }) {
+    const producto = { id_producto, nombre, precio, foto, count: 1 };
+    const indiceProductos = carrito.findIndex((item) => item.id === id);  //busca si el producto está en el carrito
+    if (indiceProductos >= 0) {
+      carrito[indiceProductos].count++; //si el producto ya existe, incrementa la cantidad del producto
+      setCarrito([...carrito]);
+    } else {
+      setCarrito([...carrito, producto]); //si no existe, lo agrega como nuevo producto al carrito
+    }
+  }
+
+  const sumarCantidad = (id) => {  // incrementa la cantidad de un producto en el carrito
+    const nuevoCarrito = carrito.map((item) =>
+      item.id === id ? { ...item, count: item.count + 1 } : item
+    );
+    setCarrito(nuevoCarrito);
+  };
+
+  const restarCantidad = (id) => { //reduce la cantidad de un producto y elimina el producto si la cantidad llega a cero
+    const nuevoCarrito = carrito.map((item) =>
+        item.id === id ? { ...item, count: item.count - 1 } : item
+      )
+      .filter((item) => item.count > 0);
+    setCarrito(nuevoCarrito);
+  };
+
+  const calcularTotal = () => { //calcula el costo total del carrito multiplicando el precio de cada producto por su cantidad (count) y sumando los resultados.
+    return carrito.reduce(
+      (total, item) => total + item.price * item.count,
+      0
+    );
+  };
+
+  return (  //se devuelve el provider  que envuelve a los childrens y proporciona los valores y funciones para que otros componentes puedan acceder a ellos
     <viveContext.Provider
       value={{
         producto,
         setProducto,
         carrito,
         setCarrito,
+        agregarCarrito,
+        sumarCantidad,
+        restarCantidad,
+        calcularTotal,
       }}
     >
       {children}
