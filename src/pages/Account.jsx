0b58ae9346e6349283485c8e03b4888cf1d029e5
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-11-10 14:49:40 -0300

    código inicial para account.jsx

---
+++ src/pages/Account.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import RegisterForm from "../components/RegisterForm";
+import LoginForm from "../components/LoginForm";
+
+const Account = () => {
+    return (
+      <div className="account-container">
+        <div className="form-container">
+          <RegisterForm />
+        </div>
+        <div className="form-container">
+          <LoginForm />
+        </div>
+      </div>
+    );
+  };
+  
+  export default Account;
